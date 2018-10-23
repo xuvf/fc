2eca4a1c93434d6da5d2fd4bd7673f7b1ac6c0e8
wxx522 <wxx522@cs.bham.ac.uk>
2018-10-23 11:59:42 +0100

    Extract UK number

--- part1/part1.py
+++ part1/part1.py
@@ -1,4 +1,26 @@
 """This module should filter and sort uk phone numbers from the text file provided. """
+import re
+import sys
 
-if __name__ == "__main__"
-    #Your code here
+
+def main(file):
+    raw_text = file.read()
+    result = []
+    
+    rule = re.compile(" ")
+    raw_text = (rule.sub('',raw_text))
+    rule = re.compile(r"\+44\d{10,}")
+    uknumber = rule.findall(raw_text)
+    for i in range(len(uknumber)):
+        if (len(uknumber[i])<14):
+            result.append(uknumber[i])        
+    
+    print(len(result))
+
+if __name__ == "__main__" :
+    try:
+        f = open(sys.argv[1],"r")
+    except IOError:
+        print("Failed to load the file")
+    else:
+        main(f)
