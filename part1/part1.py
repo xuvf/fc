92c1a60628a7bbc4ead2b5b5661ec41347ec884e
xuvf <wilson.xu.vf@gmail.com>
2018-10-23 16:17:26 +0100

    Finish Part 1

--- part1/part1.py
+++ part1/part1.py
@@ -5,22 +5,25 @@ import sys
 
 def main(file):
     raw_text = file.read()
-    result = []
-    
+    result = str()
+
     rule = re.compile(" ")
     raw_text = (rule.sub('',raw_text))
     rule = re.compile(r"\+44\d{10,}")
     uknumber = rule.findall(raw_text)
+    uknumber.sort()
     for i in range(len(uknumber)):
         if (len(uknumber[i])<14):
-            result.append(uknumber[i])
-    
-    print(len(result))
+            result += "0" + uknumber[i][3:7] + " " + uknumber[i][7:] + "\n"
+    print(result)
 
 if __name__ == "__main__" :
     try:
         f = open(sys.argv[1],"r")
+    except IndexError:
+        print("Usage: python part1.py <fileName>")
     except IOError:
         print("Failed to load the file")
     else:
         main(f)
+        f.close
